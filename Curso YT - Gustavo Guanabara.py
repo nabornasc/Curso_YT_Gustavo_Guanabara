0bab2d5a83788263636f23ec30b1f3652ba81f13
nabor do nascimento da silva <nabornasc@gmail.com>
2026-04-20 10:04:15 -0400

    exercicio type, .format & .is...

--- Curso YT - Gustavo Guanabara.py
+++ Curso YT - Gustavo Guanabara.py
@@ -1,2 +1,25 @@
-print('ola mundo BR')
-print('Parabens nova branch criada pc-servico')
+# print('ola mundo BR')
+# print('Parabens nova branch criada pc-servico')
+#
+# n1,n2=map(int,input('Digite 2 valores separado por espaço "A B", para Somar: ').split())
+#
+# soma=n1+n2
+#
+# print('A soma entre {} e {} vale {}'.format(n1,n2,soma))
+#
+# algo=input('Digite algo: ')
+#
+# print('Tipo= ',type(algo))
+# print('É numero: ',algo.isnumeric())
+# print('É alfanumerico: ',algo.isalnum())
+# print('É alfabetico: ',algo.isalpha())
+# print('É ASCII: ',algo.isascii())
+# print('É str exibivel: ',algo.isprintable())
+# print('É decimal: ',algo.isdecimal())
+# print('É um digito: ',algo.isdigit())
+# print('É um identificador: ',algo.isidentifier())
+# print('É minusculo: ',algo.islower())
+# print('É maiusculo: ',algo.isupper())
+# print('É um espaço em branco: ',algo.isspace())
+# print('É um titulo ex. Abcde: ',algo.istitle())
+
