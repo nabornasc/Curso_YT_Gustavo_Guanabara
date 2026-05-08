51a0fc1fcf246d5c4bc665a24d8a802baf1c5a29
nabor do nascimento da silva <nabornasc@gmail.com>
2026-05-08 15:36:42 -0400

    BeeCrowd 1051 - Imposto de Renda

--- Curso YT - Gustavo Guanabara.py
+++ Curso YT - Gustavo Guanabara.py
@@ -1,7 +1,7 @@
 # print('ola mundo BR')
 # print('Parabens nova branch criada pc-service')
 #
-# n1,n2=map(int,input('Digite 2 valores separado por espaço "A B", para Somar: ').split())
+# n1,n2=map(int, input('Digite 2 valores separado por espaço "A B", para somar: ').split())
 #
 # soma=n1+n2
 #
@@ -28,7 +28,7 @@
 # print('A soma dos valores {} e {} = {}'.format(n1,n2,soma))
 
 # 005ex
-# Order de precedencia 1-- (); 2-- **; 3-- *,/,//,%; 4-- +,-
+# Order de precedência 1-- (); 2-- **; 3-- *,/,//,%; 4-- +,-
 
 # print(5+2)
 # print(5-2)
@@ -43,7 +43,7 @@
 # print(3*(5+4)**2)
 
 # nome=input('Qual é seu nome?: ')
-# print('Prazer em te conhecer {:~^20}!'.format(nome)) # teste de tipos de inferencia
+# impressão('Prazer em te conhecer {:~^20}!'.format(nome)) # teste de tipos de inferência
 
 # ???ex-Beecrowd - Ordnance
 #
@@ -57,14 +57,14 @@
 # print(a)
 # print(b)
 # print(c)
-'''
-fluxo real:
 
-[a, b, c] → cria lista
-sorted(...) → ordena → [x, y, z]
-* → vira → print(x, y, z)
-sep='\n' → imprime cada valor em linha separada
-'''
+# fluxo real:
+#
+# [a, b, c] → cria lista
+# sorted(...) → ordena → [x, y, z]
+# * → vira → print(x, y, z)
+# sep='\n' → imprime cada valor em linha separada
+
 # a, b, c = map(int, input().split())
 #
 # print(*sorted([a, b, c]), sep='\n')
@@ -79,21 +79,21 @@ sep='\n' → imprime cada valor em linha separada
 # div=n1/n2
 # divInt=n1//n2
 # exp=n1**n2
-# print('A soma é {}, o produce é {} \ne a divisão é {:.2f}'.format(soma,mult,div),end=' ')
-# print('e a Divisão Inteira é {} e a potencia é {}'.format(divInt,exp))
+# impressão('A soma é {}, o produce é {} \ne a divisão é {:.2f}'.format(soma,mult,div),end=' ')
+# impressão('e a Divisão Inteira é {} e a potência é {}'.format(divInt,exp))
 
-# # 005desafio - da para colocar o calculo dentro do .format
+# # 005desafio - da para colocar o cálculo dentro do.format
 # num=int(input('Digite um numero integer: '))
 # sucessor=num+1
 # antecessor=num-1
-# print('O numero digitado foi {}, seu successor é {} e o antecessor é {}'.format(num,successor,antecessor))
+# impressão('O número digitado foi {}, o seu successor é {} e o antecessor é {}'.format(num,successor,antecessor))
 
 # # 006desafio
-# num=int(input('Digite um numero integer:'))
+# num=int(input('Digite um numero integer: '))
 # dobro=num*2
 # triplo=num*3
 # raiz=num**(1/2)
-# print('O numero digitado foi {:~^10}, e seu dobro é {:~^10}\nseu Triplo é {:~^10} e a sua raiz Quadrada {:~^10.2f}'.format(num,dobro,triplo,raiz))
+# impressão('O número digitado foi {:~^10}, e o seu dobro é {:~^10}\nseu Triplo é {:~^10} e a sua raiz Quadrada {:~^10.2f}'.format(num,dobro,triplo,raiz))
 
 # # 007desafio
 # nota1=float(input('Digite a 1 nota: '))
@@ -118,15 +118,15 @@ sep='\n' → imprime cada valor em linha separada
 # dinheiro=float(input('Digite o valor do dinheiro: '))
 # dolar=float(input('Digite o valor do dólar: '))
 # conversao=dinheiro/dolar
-# print('Com R$ {:^10,.2f} , voce tera $ {:^10,.2f} dólar ao preço de: {:^10,.2f}.'.format(dinheiro,conversao,dólar))
+# impressão('Com R$ {:^10,.2f}, você tera $ {:^10,.2f} dólar ao preço de: {:^10,.2f}.'.format(dinheiro,conversao,dólar))
 
 # 011 dessafio
 # largura=float(input('Digite a Largura da pared em Mts: '))
 # altura=float(input('Digite a Altura da pared em Mts: '))
-# quantM2=float(input('Qual rendimiento da tinta em M²: '))
+# quantM2=float(input('Qual rendimento da tinta em M²: '))
 # metrosQuad=largura*altura
 # areaPintada=metrosQuad/quantM2
-# print('Para uma pared {} por {}, da um total de {}m². Que usará {}Litros com {}m² rendimento'.format(largura,altura,metrosQuad,areaPintada,quantM2))
+# impressão('Para uma pared {} por {}, da um total de {}m². Que usará {}Litros com {}m² rendimento'.format(largura,altura,metrosQuad,areaPintada,quantM2))
 #
 # v2 - sugestao cloude
 # Desafio 011 - Cálculo de tinta
@@ -139,25 +139,25 @@ sep='\n' → imprime cada valor em linha separada
 # metrosQuad = largura * altura
 # areaPintada = metrosQuad / rendEmba
 #
-# print(f'\nPara uma parede de {largura}m x {altura}m:')
-# print(f'  Área total:       {metrosQuad:.2f} m²')
-# print(f'  Tinta necessária: {areaPintada:.2f} Litros')
-# print(f'  Rendimento:       {rendEmba} m²/L')
+# print(f'\nPara uma parede de {largura}m x {altura}m: ')
+# print(f' Área total: {metrosQuad:.2f} m²')
+# print(f' Tinta necessária: {areaPintada:.2f} Litros')
+# print(f' Rendimento:       {rendEmba} m²/L')
 
 # 012 desafío
 # preco=float(input('Digite um preço: R$'))
 # desconto=float(input('Digite o desconto: '))
 # # novoPreco=preco*(1-desconto/100)
-# novoPreco=preco-(preco*desconto/100) # Outro metodo para chegar no resultado
-# print(f'O novo preço de R${preco:.2f}, com desconto {desconto:.2f}% é: R${novoPreco:.2f}')
+# novoPreco=preco-(preco*desconto/100) # Outro método para chegar no resultado
+# impressão(f'O novo preço de R${preco:.2f}, com desconto {desconto:.2f}% é: R${novoPreco:.2f}')
 
 # 013 desafío
-# salario=float(input('Digite seu salario: '))
+# salario=float(input('Digite seu salário: '))
 # aumento=float(input('Digite aumento %: '))
-# novoSalario=salario*(1+aumento/100)
-# print(f'Seu novo salario é: R${novoSalario:.2f}')
+# novoSalario=salário*(1+aumento/100)
+# print(f'Seu novo salário é: R${novoSalario:.2f}')
 
-# 014 desasfio
+# 014 desafio
 
 # temp=float(input('Digite a temperatura em Celsius: '))
 # fahr=(temp*9/5)+32
@@ -187,9 +187,9 @@ sep='\n' → imprime cada valor em linha separada
 # opc3=input()
 #
 # list1=['vertebrado','invertebrado']
-# list2=['ave','mamifero','inseto','anelideo']
-# list3=['carnivoro','onivoro','herbivoro','hematofago']
-# list4=['aguia','pomba','homem','vaca','pulga','lagarta','sanguessuga','minhoca']
+# list2=['ave','mamífero','inseto','anelídeo']
+# list3=['carnívoro','onivoro','herbívoro','hematofago']
+# list4=['águia','pomba','homem','vaca','pulga','lagarta','sanguessuga','minhoca']
 #
 # if opc1 == list1[0]:
 #     if opc2 == list2[0]:
@@ -217,3 +217,23 @@ sep='\n' → imprime cada valor em linha separada
 #         else:
 #             print(list4[7])
 
+salario = float(input())
+
+if salario <= 2000.00:
+    print("Isento")
+else:
+    imposto = 0.0
+
+    if salario > 2000.00:
+        faixa1 = min(salario, 3000.00) - 2000.00
+        imposto += faixa1 * 0.08
+
+    if salario > 3000.00:
+        faixa2 = min(salario, 4500.00) - 3000.00
+        imposto += faixa2 * 0.18
+
+    if salario > 4500.00:
+        faixa3 = salario - 4500.00
+        imposto += faixa3 * 0.28
+
+    print(f"R$ {imposto:.2f}")
