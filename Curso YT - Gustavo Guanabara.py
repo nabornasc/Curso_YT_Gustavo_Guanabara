455f92eacb25c2856a4ca5728fd711f26c13b24a
nabor do nascimento da silva <nabornasc@gmail.com>
2026-05-11 17:48:31 -0400

    teste exercicio 2 - aula 5_5

--- Curso YT - Gustavo Guanabara.py
+++ Curso YT - Gustavo Guanabara.py
@@ -217,23 +217,24 @@
 #         else:
 #             print(list4[7])
 
-salario = float(input())
+# salario = float(input())
+#
+# if salario <= 2000.00:
+#     print("Isento")
+# else:
+#     imposto = 0.0
+#
+#     if salario > 2000.00:
+#         faixa1 = min(salario, 3000.00) - 2000.00
+#         imposto += faixa1 * 0.08
+#
+#     if salario > 3000.00:
+#         faixa2 = min(salario, 4500.00) - 3000.00
+#         imposto += faixa2 * 0.18
+#
+#     if salario > 4500.00:
+#         faixa3 = salario - 4500.00
+#         imposto += faixa3 * 0.28
+#
+#     print(f"R$ {imposto:.2f}")
 
-if salario <= 2000.00:
-    print("Isento")
-else:
-    imposto = 0.0
-
-    if salario > 2000.00:
-        faixa1 = min(salario, 3000.00) - 2000.00
-        imposto += faixa1 * 0.08
-
-    if salario > 3000.00:
-        faixa2 = min(salario, 4500.00) - 3000.00
-        imposto += faixa2 * 0.18
-
-    if salario > 4500.00:
-        faixa3 = salario - 4500.00
-        imposto += faixa3 * 0.28
-
-    print(f"R$ {imposto:.2f}")
